c09c4d7b71c8c02927acb98c6cf7a989f76dd0fb
Kewillem Nikoly <kewillemg54@gmail.com>
2023-06-13 11:35:57 -0400

    13-06-23

--- js/scriptSalgado.js
+++ js/scriptSalgado.js
@@ -14,4 +14,61 @@ function validaCampo(campo){
             msnErro.textContent="";
         }
     }
+    if(campo.name === "descricao"){
+        if(campo.value.length<5){
+            msnErro.textContent = "Digite uma descrição prévia da receita";
+        }else{
+            msnErro.textContent="";
+        }
+    }
+    if(campo.name === "ingrediente"){
+        if(campo.value.length<5){
+            msnErro.textContent = "Digite os ingredientes necessários na receita";
+        }else{
+            msnErro.textContent="";
+        }
+    }
+    if(campo.name === "passos"){
+        if(campo.value.length<5){
+            msnErro.textContent = "Digite o passo a passo da receita";
+        }else{
+            msnErro.textContent="";
+        }
+    }
+    if(campo.name === "informacao"){
+        if(campo.value.length<5){
+            msnErro.textContent = "Digite as informações adicionais da receita";
+        }else{
+            msnErro.textContent="";
+        }
+    }
 }
+
+if (document.getElementById("Tipo").value == "0"){
+    msnErro.textContent = "Selecione um campo";
+}else{
+    msnErro.textContent="";
+}
+
+// function validar() {
+//     var facil = fOcorrencia.facil.value;
+//     var medio = fOcorrencia.medio.value;
+//     var dificil = fOcorrencia.dificil.value;
+    
+//     //captura o número de itens "selecionados"
+//     var Tipo = document.querySelectorAll('input[name="Tipo"]:checked').length;    
+  
+//     if (facil == "") {
+//         alert("Preencha todos os campos");
+//         return false;
+//     }
+//     if (medio == "") {
+//         alert("Preencha todos os campos");
+//         return false;
+//     }
+//     if (dificil == "") {
+//         alert("Preencha todos os campos");
+//         return false;
+//     }
+//     return true;
+// }
