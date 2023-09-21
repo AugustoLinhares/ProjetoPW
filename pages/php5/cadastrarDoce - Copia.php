f0e352f01650b8e52dd11439a4ad13ac5bd9c319
Kewillem Nikoly <kewillemg54@gmail.com>
2023-09-21 15:21:12 -0400

    21-09-23

--- pages/php5/cadastrarDoce - Copia.php
+++ pages/php5/cadastrarDoce - Copia.php
@@ -2,10 +2,14 @@
 $_conn = mysqli_connect('127.0.0.1','root','','php5');
 if($_conn===FALSE) {
  echo "Não foi possível conectar ao Servidor de banco de dados ";
- echo "Erro Dados";
-} 
-else {
-      
+} else {
+ echo "Foi possível conectar ao Servidor de banco de dados ";
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && $_SERVER["REQUEST_METHOD"] == "FILES") {
+
+    require_once('php5.php');
+    
     $Titulo = $_POST["Titulo"];
     $Adicional = $_POST["Adicional"];
     $Descricao = $_POST["Descricao"];
@@ -15,8 +19,14 @@ else {
     $Dificuldade = $_POST["Dificuldade"];
     $Porcao = $_POST["Porcao"];
     $Passo = $_POST["Passo"];
+    $Fk = $_POST["id_usu_fk"];
     
-    $sql = "INSERT INTO receita (Titulo, Adicional,	Descricao,	Ingrediente, TempoPrep,	Dificuldade, Porcao, Passo) VALUES ('$Titulo', '$Adicional', '$Descricao', '$Ingrediente', '$TempoPrep', '$Dificuldade', '$Porcao', '$Passo');";
+
+ 
+
+    $sql = "INSERT INTO receita VALUES (null, '$Titulo', '$Adicional', '$Descricao', '$Ingrediente', '$TempoPrep', '$Dificuldade', '$Porcao', '$Passo', '$Fk');";
+
+
     
     if ($_conn->query($sql) === TRUE) {
         echo"Dados inseridos com sucesso!";
@@ -26,6 +36,8 @@ else {
     }
 
     $_conn->close();
+}else{
+    echo "Erro Dados";
 }
 ?>
 
